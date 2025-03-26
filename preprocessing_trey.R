2d822857149232e6f19042173235aed0f14e845d
Trey Turner <turnert01@tamu.edu>
2025-03-26 17:56:59 -0500

    comment out things

--- preprocessing_trey.R
+++ preprocessing_trey.R
@@ -82,92 +82,97 @@ table(treyPreprocessingFactors$application_type)
 
 #Impute missing values in numeric variables 
 #Impute missing values 
-XimputeMedian = preProcess(treyPreprocessingNumeric,
-                           method = 'medianImpute',
-                           k = 5)
-NumericNoMissing = predict(XimputeMedian,
+#XimputeMedian = preProcess(treyPreprocessingNumeric,
+                          # method = 'medianImpute',
+                           #k = 5)
+#NumericNoMissing = predict(XimputeMedian,
                            treyPreprocessingNumeric)
-anyNA(NumericNoMissing)
+#anyNA(NumericNoMissing)
 
+treyNumericWithMissing = treyPreprocessingNumeric
 #Analyze variance...also center and scale
-nearZeroVar(NumericNoMissing,
+nearZeroVar(treyNumericWithMissing,
             saveMetrics = T)
-treyPreprocessingNumericWVariance = NumericNoMissing %>% 
+treyPreprocessingNumericWVariance = treyNumericWithMissing %>% 
   preProcess(method = c('nzv')) %>%
-  predict(newdata = NumericNoMissing)
+  predict(newdata = treyNumericWithMissing)
 
-dim(treyPreprocessingNumericWVariance)
+dim(treyNumericWithMissing)
+
+treyPreprocessedData = treyNumericWithMissing 
+save(treyPreprocessedData,
+     file="treyPreprocessedData.RData")
 
 #6 variables dropped due to near zero variance 
 
 
 #Look at # of NAs in each of the numeric variables
-sum(is.na(treyPreprocessingNumericWVariance$total_rec_prncp))
-sum(is.na(treyPreprocessingNumericWVariance$total_rec_int))
-sum(is.na(treyPreprocessingNumericWVariance$recoveries))
-sum(is.na(treyPreprocessingNumericWVariance$last_pymnt_amnt))
-
-#Now analyze skew 
-skewnessVector = treyPreprocessingNumericWVariance %>%
-  sapply(e1071::skewness,na.rm=T)
-
-names(treyPreprocessingNumericWVariance)[abs(skewnessVector)> 1.5]
-
-#Plot those with absolute skew > 1.5 
-hist(treyPreprocessingNumericWVariance$total_rec_int,
-     main = "Histogram",
-     xlab = "Total Interest Received To Date")
-hist(treyPreprocessingNumericWVariance$recoveries,
-     main = "Histogram",
-     xlab = "Post Charge Off Gross Recovery")
-hist(treyPreprocessingNumericWVariance$last_pymnt_amnt,
-     main = "Histogram",
-     xlab = "Last Total Payment Amount Received")
-
-#Perform Yeo-Johnson transformation 
-YJTransformedVariables = treyPreprocessingNumericWVariance%>%
-  select(c("total_rec_int",
-           "recoveries",
-           "last_pymnt_amnt"))%>%
-  preProcess(method = 'YeoJohnson') %>%
-  predict(newdata=treyPreprocessingNumericWVariance %>%
-            select(c("total_rec_int",
-                     "recoveries",
-                     "last_pymnt_amnt")))
-
-#Check histograms 
-hist(YJTransformedVariables$total_rec_int,
-     main = "Histogram",
-     xlab = "YJ Total Interest Received To Date")
-hist(YJTransformedVariables$recoveries,
-     main = "Histogram",
-     xlab = "YJ Post Charge Off Gross Recovery")
-hist(YJTransformedVariables$last_pymnt_amnt,
-     main = "Histogram",
-     xlab = "YJ Last Total Payment Amount Received")
-
-
-#Recoveries looks odd 
-summary(treyPreprocessingNumericWVariance$recoveries)
-sum(treyPreprocessingNumericWVariance$recoveries > 0,
-    na.rm=T)
-
-
-#Look for extreme values 
-pcaOut = prcomp(YJTransformedVariables)
-
-YeoJscores = data.frame(pcaOut$x)
-ggplot(data = YeoJscores) + 
-  geom_point(aes(x = PC1, y = PC2))
-
-
-#2 extreme observations 
-extremeObs1 = which.min(YeoJscores$PC1)
-treyPreprocessingNumericWVariance[extremeObs1,]
-
-
-extremeObs2 = which.min(YeoJscores$PC2)
-treyPreprocessingNumericWVariance[extremeObs2,]
-
-
-#Do correlation filtering once we get all numeric variables together? 
+# sum(is.na(treyPreprocessingNumericWVariance$total_rec_prncp))
+# sum(is.na(treyPreprocessingNumericWVariance$total_rec_int))
+# sum(is.na(treyPreprocessingNumericWVariance$recoveries))
+# sum(is.na(treyPreprocessingNumericWVariance$last_pymnt_amnt))
+# 
+# #Now analyze skew 
+# skewnessVector = treyPreprocessingNumericWVariance %>%
+#   sapply(e1071::skewness,na.rm=T)
+# 
+# names(treyPreprocessingNumericWVariance)[abs(skewnessVector)> 1.5]
+# 
+# #Plot those with absolute skew > 1.5 
+# hist(treyPreprocessingNumericWVariance$total_rec_int,
+#      main = "Histogram",
+#      xlab = "Total Interest Received To Date")
+# hist(treyPreprocessingNumericWVariance$recoveries,
+#      main = "Histogram",
+#      xlab = "Post Charge Off Gross Recovery")
+# hist(treyPreprocessingNumericWVariance$last_pymnt_amnt,
+#      main = "Histogram",
+#      xlab = "Last Total Payment Amount Received")
+# 
+# #Perform Yeo-Johnson transformation 
+# YJTransformedVariables = treyPreprocessingNumericWVariance%>%
+#   select(c("total_rec_int",
+#            "recoveries",
+#            "last_pymnt_amnt"))%>%
+#   preProcess(method = 'YeoJohnson') %>%
+#   predict(newdata=treyPreprocessingNumericWVariance %>%
+#             select(c("total_rec_int",
+#                      "recoveries",
+#                      "last_pymnt_amnt")))
+# 
+# #Check histograms 
+# hist(YJTransformedVariables$total_rec_int,
+#      main = "Histogram",
+#      xlab = "YJ Total Interest Received To Date")
+# hist(YJTransformedVariables$recoveries,
+#      main = "Histogram",
+#      xlab = "YJ Post Charge Off Gross Recovery")
+# hist(YJTransformedVariables$last_pymnt_amnt,
+#      main = "Histogram",
+#      xlab = "YJ Last Total Payment Amount Received")
+# 
+# 
+# #Recoveries looks odd 
+# summary(treyPreprocessingNumericWVariance$recoveries)
+# sum(treyPreprocessingNumericWVariance$recoveries > 0,
+#     na.rm=T)
+# 
+# 
+# #Look for extreme values 
+# pcaOut = prcomp(YJTransformedVariables)
+# 
+# YeoJscores = data.frame(pcaOut$x)
+# ggplot(data = YeoJscores) + 
+#   geom_point(aes(x = PC1, y = PC2))
+# 
+# 
+# #2 extreme observations 
+# extremeObs1 = which.min(YeoJscores$PC1)
+# treyPreprocessingNumericWVariance[extremeObs1,]
+# 
+# 
+# extremeObs2 = which.min(YeoJscores$PC2)
+# treyPreprocessingNumericWVariance[extremeObs2,]
+# 
+# 
+# #Do correlation filtering once we get all numeric variables together? 
