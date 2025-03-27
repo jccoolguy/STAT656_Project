d11dd70a37032c05e1642c54465c9dc6d5c70222
jccoolguy PC <jgavc1@gmail.com>
2025-03-26 23:39:17 -0400

    employment length column set as factor - Jack

--- emp_length_fix.R
+++ emp_length_fix.R
@@ -1,31 +1,26 @@
 load("emp_length.Rdata")
 
-#Looking at current status of variable
-emp_length |> 
-  group_by(emp_length_no_year) |> 
-  summarise(n = n())
-emp_vector = as.vector(emp_length$emp_length_no_year)
+#Unique values and counts
+table(emp_length)
 
-#Dealing with strings
-emp_vector_missings = ifelse(emp_vector == "n/a"|emp_vector == "","Unknown Job History",emp_vector)
-emp_vector_less_than_one = ifelse(emp_vector_missings == "< 1 ","Short Job History",emp_vector_missings)
-emp_vector_high_experience = ifelse(emp_vector_less_than_one == "10+ ", "Very Long Job History", emp_vector_less_than_one)
+#Removing year/years
+emp_length_no_years = gsub(" year| years", "",as.vector(emp_length$emp_length))
+table(emp_length_no_years)
 
-#Dealing with numbers
+#Creating factor levels
+emp_length_factor = as.factor(ifelse(emp_length_no_years == "< 1",
+                              "Minimal",
+                              ifelse(emp_length_no_years == "n/a"|emp_length_no_years == "",
+                                     "Unknown",
+                                     ifelse(emp_length_no_years == "10+",
+                                            "Very Long",
+                                            ifelse(emp_length_no_years %in% c("1","2","3"),
+                                                   "Short", ifelse(emp_length_no_years %in% c("4","5","6"),
+                                                                   "Moderate",ifelse(emp_length_no_years %in% c("7","8","9"),
+                                                                                     "Long","")))))))
+#Verifying counts
+table(emp_length_factor)
 
-#Removing spaces
-emp_vector_no_space = emp_vector_high_experience = sub(" ","",emp_vector_high_experience)
-
-#Creating moderate and long history
-strings = c("Unknown Job History","Short Job History", "Very Long Job History")
-emp_vector_moderate = ifelse((!emp_vector_no_space %in% strings) & as.numeric(emp_vector_no_space) >= 1 & as.numeric(emp_vector_no_space) <= 5,"Moderate Job History",
-                             emp_vector_no_space)
-
-
-
-
-#emp_vector_low = ifelse(emp_vector_missings == "< 1 "|emp_vector <= 3,"Low Experience",emp_vector_missings)
-#emp_vector_high = ifelse(emp_vector_moderate == "10+ ","High Experience", emp_vector_moderate)
-#emp_vector_moderate = ifelse(emp_vector_low >= 3 & emp_vector <= 10, "Moderate Experience",emp_vector_low)
-#emp_vector_high = ifelse(emp_vector_moderate == "10+ ","High Experience", emp_vector_moderate)
-#emp_vector_high
+#Saving Down Object
+save(emp_length_factor,
+     file = "emp_length_factor")
